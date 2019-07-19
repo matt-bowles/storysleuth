20287fa5db38cd231088ea33667f724541403927
Matt Bowles <mattbowles1337@gmail.com>
2019-07-19 20:23:33 +1000

    The big update

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 const cities = require('./cities.json');
 
+const RADIUS = 3000;   // in metres
+const ZOOM = 2;        // between 2-18
+const MIN_STORIES = 3; // the min. amount of stories a successful playlist must have
+
 
 app.set('view engine', 'ejs');
 
@@ -12,35 +16,100 @@ app.set('view engine', 'ejs');
 
 // home route
 app.get('/', (req, res) => {
+  // var data = getStories();9
+  console.log();
+  city = getCity();
 
-  var totalStories = 0;
+  // Get stories for city
+  playlist = getStory(res, city);
 
-  console.log("-----------------------------");
-  console.log("--Looking for some stories!--");
-  console.log("-----------------------------");
-  
-  do {
-    var city = cities[Math.floor(Math.random()*cities.length)];
-    console.log(city);
-    snapMap.getPlaylist(city.lat, city.lng, 10000, 2).then(function(playlist) {
-      // return res.send(playlist);
-      if (!playlist.totalCount) {
-        console.log("Breaking!");
-        return
-      } else {
-        console.log("We shouldn't get here");
-        console.log(playlist.totalCount + " stories found!");
-
-        var story = playlist.elements[Math.floor(Math.random()*playlist.totalCount)];
-        
-        var storyURL = story.snapInfo.streamingMediaInfo.prefixUrl + story.snapInfo.streamingMediaInfo.mediaWithOverlayUrl;
-      
-        var timestamp = parseInt(new Date().getTime() - story.timestamp)/60/1000;
-        return res.render('home', {storyURL: storyURL, timestamp: timestamp}); 
-      }
-       
-     });
-  } while (isNaN(totalStories) && totalStories != undefined && totalStories>0);
+  // return res.render('home', {storyURL: "storyURL", timestamp: timestamp}); 
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+function getStory(res, city) {
+  snapMap.getPlaylist(city.lat, city.lng, RADIUS, ZOOM)
+    .then((pl) => { 
+      console.log(pl.totalCount + " stories found!");
+      if (!isNaN(pl.totalCount) && pl.totalCount >= MIN_STORIES) {
+        console.log("Doing shit...");
+        console.log(city.lat + ", " + city.lng);
+        doShit(pl, res);
+      }
+
+      // Story is rejected
+      else {
+        // Delete city from cities.json
+        // cities.filter(function(data) {
+        //   return data.id !== city.id;
+        // });
+
+        // Find new city
+        console.log("Finding a new city...");
+        newCity = getCity();
+        getStory(res, newCity);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getCity() {
+  // Find city and print it
+  var city = cities[Math.floor(Math.random()*cities.length)];
+  console.log(city.city + ", " + city.country);
+  return city;
+}
+
+function deleteCity(city) {
+  console.log(cities.filter(function(data) {
+    data.id == city.id;
+  }));
+  
+}
+
+function doShit(playlist, res) {
+  // Get random story from playlist
+  var story = playlist.elements[Math.floor(Math.random()*playlist.totalCount)];
+
+  try {
+  var suffix = (story.snapInfo.streamingMediaInfo.mediaWithOverlayUrl) ? story.snapInfo.streamingMediaInfo.mediaWithOverlayUrl : story.snapInfo.streamingMediaInfo.mediaUrl;
+  } catch (err) {
+    console.log("wtf happened bro");
+    console.log("Finding a new city...");
+    city = getCity();
+    return getStory(res, city);
+  }
+  var storyURL = story.snapInfo.streamingMediaInfo.prefixUrl + suffix;
+
+  // How long ago was the story posted?
+  var timestamp = timeSince(story.timestamp);
+
+  console.log(city.city + ", " + city.country + " - " + playlist.totalCount + " stories found!");
+
+  // return res.send(playlist); 
+  return res.render('home', {storyURL: storyURL, timestamp: timestamp, lat: city.lat, long: city.lng}); 
+}
+
+function timeSince(timeStamp) {
+  var now = new Date(),
+    secondsPast = (now.getTime() - timeStamp) / 1000;
+  if(secondsPast < 60){
+    return parseInt(secondsPast) + ' seconds ago';
+  }
+  if(secondsPast < 3600){
+    return parseInt(secondsPast/60) + ' minutes ago';
+  }
+  if(secondsPast <= 86400){
+    return parseInt(secondsPast/3600) + ' hours ago';
+  }
+  if(secondsPast > 86400){
+      day = timeStamp.getDate();
+      month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ","");
+      year = timeStamp.getFullYear() == now.getFullYear() ? "" :  " "+timeStamp.getFullYear();
+      return day + " " + month + year;
+  }
+}
+
+app.listen(3000, () => console.log('The application is running on localhost:3000!'));
