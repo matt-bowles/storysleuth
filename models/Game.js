02fa89e24089cbc24d70ff1160a8491e38ad33d0
matt-bowles <mattbowles1337@gmail.com>
2020-01-11 19:47:56 +1000

    Added game registering/saving

--- models/Game.js
+++ models/Game.js
@@ -1,32 +1,71 @@
 const mongoose = require('mongoose');
 const Playlist = require('./Playlist');
 
+// Coords schema
+const coordsSchema = mongoose.Schema({
+  lat: {
+    type: Number,
+    required: true
+  },
+  lng: {
+    type: Number,
+    required: true
+  }
+})
+
+// Story schema
+const storySchema = mongoose.Schema({
+  timestamp: {
+    type: String,
+    required: true
+  },
+  storyURL: {
+    type: String,
+    required: true
+  },
+  isImage: {
+    type: Boolean,
+    required: true
+  }
+})
+
+// Round schema
+const roundSchema = mongoose.Schema({
+  coords: coordsSchema,
+  stories: [storySchema]
+})
+
 // Game schema
 const gameSchema = mongoose.Schema({
   id: {
     type: Number,
     required: true
-  }
+  },
+  rounds: [roundSchema]
 });
 
 const Game = module.exports = mongoose.model('Game', gameSchema);
 
 // Generate a game
-module.exports.generateGame = (req, numPlaylists=5) => {
+module.exports.generateGame = (req, numRounds=5) => {
   return new Promise(function(resolve, reject) {
     
-    var promises = []; 
+    var rounds = []; 
 
-    for (i=0; i<numPlaylists; i++) {
-      promises.push(Playlist.getPlaylist(req.query, 0));
+    for (i=0; i<numRounds; i++) {
+      rounds.push(Playlist.getPlaylist(req.query, 0));
     }
 
     console.log();
 
-    Promise.all(promises).then((game) => {
+    Promise.all(rounds).then((game) => {
+      game.id = 1;
       resolve(game);
 
       // Save game to db here
+      addGame(game, (err) => {
+        if (err) console.log(err);
+      })
     })
   });
 }
@@ -37,6 +76,14 @@ module.exports.getGameById = (id, callback) => {
 }
 
 // Add a game
-module.exports.addGame = (game, callback) => {
-  Game.create(game, callback);
+function addGame(game, callback) {
+  var g = {};
+  g.id = game.id;
+  g.rounds = [];
+
+  game.forEach(round => {
+    g.rounds.push(round);
+  });
+
+  Game.create(g, callback);
 }
